b7fe211942d4ba7a83e0883262f5c3b7831efa66
mmgmolotov <mmgmolotov@gmail.com>
2023-11-12 21:04:18 +0100

    fix 0.6

--- models/state.py
+++ models/state.py
@@ -7,3 +7,11 @@ This Module contains a definition for State Class
 from models.base_model import BaseModel
 
 
+class State(BaseModel):
+    """A class that represents a state
+
+    Attribute:
+        name (str): the name of the state
+    """
+
+    name = ""
